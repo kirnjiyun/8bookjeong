e411fd12095c15d087896f42b53bca92cce233ef
kirnjiyun <kimjiyunee@naver.com>
2024-08-30 21:57:21 +0900

    로딩 디자인 변경

--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import * as S from './loading.styled';
 
 const Loading = () => {
-  return <S.IsLoading />;
+  return (
+    <S.IsLoading>
+      <svg viewBox='0 0 50 50'>
+        <circle cx='25' cy='25' r='20' fill='none' strokeWidth='5'></circle>
+      </svg>
+    </S.IsLoading>
+  );
 };
 
 export default Loading;
--- src/components/common/Loading/loading.styled.js
+++ src/components/common/Loading/loading.styled.js
@@ -1,38 +1,47 @@
 import styled, { keyframes } from 'styled-components';
 import { primaryColor, secondaryColor } from '../../../assets/style/globalStyle.styled';
 
-const ani = keyframes`
-  50%,100% {clip-path: inset(0 -20% 0 0)}
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 `;
-export const IsLoading = styled.span`
-  display: inline-grid;
-  // position: absolute;
+
+const dash = keyframes`
+  0% {
+    stroke-dasharray: 1, 150;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -124;
+  }
+`;
+
+export const IsLoading = styled.div`
+  display: inline-block;
   position: fixed;
   left: 50%;
-  // top: 50%;
-  top: 44%;
+  top: 50%;
   transform: translate(-50%, -50%);
   z-index: 90;
 
-  &:before,
-  &:after {
-    content: '';
-    grid-area: 1/1;
-    height: 27px;
-    aspect-ratio: 5;
-    --c: ${primaryColor} 90deg, #0000 0;
-    background:
-      conic-gradient(from 135deg at top, var(--c)),
-      conic-gradient(from -45deg at bottom, var(--c)) 12.5% 100%;
-    background-size: 20% 50%;
-    background-repeat: repeat-x;
-    -webkit-mask: repeating-linear-gradient(90deg, ${primaryColor} 0 10%, #0000 0 20%);
-    clip-path: inset(0 100% 0 0);
-    animation: ${ani} 1.5s infinite steps(6);
+  & > svg {
+    animation: ${rotate} 2s linear infinite;
+    width: 50px;
+    height: 50px;
   }
 
-  &:after {
-    -webkit-mask: repeating-linear-gradient(90deg, #0000 0 10%, #000 0 20%);
-    animation-delay: -0.75s;
+  & > svg circle {
+    stroke: ${secondaryColor};
+    stroke-linecap: round;
+    animation: ${dash} 1s ease-in-out infinite;
   }
 `;
