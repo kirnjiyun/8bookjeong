f796122aac0f9f429f185efad72d6ce4ea4aa8c6
kirnjiyun <kimjiyunee@naver.com>
2024-08-30 17:25:33 +0900

    cloudtype 주석처리 ,apikey변경

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,30 @@
 import axios from 'axios';
-const API_KEY = 'ttbdudgh46512353001';
+// const API_KEY = 'ttbdudgh46512353001';
 
-const api = axios.create({
-  baseURL: 'https://port-0-bookj-backend-rm6l2llwasdocy.sel5.cloudtype.app/',
-  params: {
-    TTBKey: API_KEY,
-    output: 'js',
-    Version: '20131101',
-    Cover: 'Big'
-  },
-  withCredentials: true
-});
-
-export default api;
-
-//import axios from 'axios';
-// const API_KEY = process.env.REACT_APP_API_KEY;
-// const BASE_URL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'http://www.aladin.co.kr/ttb/api' // 운영 환경에서 사용할 API의 기본 URL
-//     : '/ttb/api'; // 개발 환경에서 사용할 API의 기본 URL
 // const api = axios.create({
-//   baseURL: BASE_URL,
+//   baseURL: 'https://port-0-bookj-backend-rm6l2llwasdocy.sel5.cloudtype.app/',
 //   params: {
 //     TTBKey: API_KEY,
 //     output: 'js',
 //     Version: '20131101',
 //     Cover: 'Big'
-//   }
+//   },
+//   withCredentials: true
 // });
 
 // export default api;
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = 'http://www.aladin.co.kr/ttb/api';
+console.log(API_KEY);
+const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    TTBKey: API_KEY,
+    output: 'js',
+    Version: '20131101',
+    Cover: 'Big'
+  }
+});
+
+export default api;
